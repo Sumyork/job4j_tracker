1f20d55a1478e2084b2d69de65cd4d9b0df5b3db
Sumyork <psychohistorian.p@gmail.com>
2024-02-05 14:33:24 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789 #479138] - исправлен

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -21,7 +21,7 @@ public class StartUI {
         while (run) {
             showMenu(actions);
             int select = input.askInt("Выбрать: ");
-            if (select < 0 || select > actions.length) {
+            if (select < 0 || select >= actions.length) {
                 output.println("Неверный ввод, вы можете выбрать: 0 .. " + (actions.length - 1));
                 continue;
             }
@@ -39,7 +39,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ValidateInput();
+        Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new Create(output),

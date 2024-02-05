4f61b703089414a3a05510e0fb64587f884ef13a
Sumyork <psychohistorian.p@gmail.com>
2024-02-05 13:30:21 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -1,27 +1,25 @@
 package ru.job4j.ex;
 
-import ru.job4j.tracker.Item;
-
-import java.util.Arrays;
-
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        for (int index = 0; index < users.length; index++) {
-            User user = users[index];
-            if (login.equals(user.getUsername())) {
-                users[index] = user;
-                return user;
-            } else {
-                throw new UserNotFoundException("There is no such user.");
+        User user = null;
+        for (User value : users) {
+            if (login.equals(value.getUsername())) {
+                user = value;
+                break;
             }
         }
-        return null;
+        if (user == null) {
+            throw new UserNotFoundException("There is no such user.");
+        }
+        return user;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
         if (!user.isValid()) {
             throw new UserInvalidException("The user is not valid");
-        } else if (user.getUsername().length() < 3) {
+        }
+        if (user.getUsername().length() < 3) {
             throw new UserInvalidException("Username less than 3 letters.");
         }
         return true;

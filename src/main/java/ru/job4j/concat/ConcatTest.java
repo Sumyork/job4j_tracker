ea7559da3719afa6fabdb8a5877a1787e0f0100d
Sumyork <psychohistorian.p@gmail.com>
2024-02-11 02:11:31 +0300

    1. Уникальные задачи. [#212653] v.4

--- src/main/java/ru/job4j/concat/ConcatTest.java
+++ src/main/java/ru/job4j/concat/ConcatTest.java
@@ -3,9 +3,9 @@ package ru.job4j.concat;
 public class ConcatTest {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        String string = "Job4j";
+        StringBuilder string = new StringBuilder("Job4j");
         for (int index = 0; index < 9999; index++) {
-            string = string + index;
+            string.append(index);
         }
         System.out.println("Миллисекунд: " + (System.currentTimeMillis() - startTime));
     }

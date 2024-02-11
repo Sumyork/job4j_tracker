2b80818228d13ec498318f90a553cb06a5e5dd4a
Sumyork <psychohistorian.p@gmail.com>
2024-02-11 20:49:05 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("qqqq1234@mail.ru", "Ivanov Ivan");
+        hashMap.put("kastruly@mail.ru", "Kastruly Julia");
+        hashMap.put("taras@mail.ru", "Don Taras");
+        hashMap.put("qazwsx@mail.ru", "Bez Prekras");
+        hashMap.put("foodmaster@mail.ru", "Albedo BezObeda");
+        for (String key : hashMap.keySet()) {
+            String value = hashMap.get(key);
+            System.out.println(key + " --- " + value);
+        }
+    }
+}

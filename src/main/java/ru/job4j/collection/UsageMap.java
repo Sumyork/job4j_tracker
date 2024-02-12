1b9a01fca8c321b05c572fb5c7ee7d705b63dc91
Sumyork <psychohistorian.p@gmail.com>
2024-02-12 13:02:52 +0300

    0. Map, HashMap [#212652] - исправил

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,11 +5,13 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("qazwsx@mail.ru", "Bez Prekras");
         hashMap.put("qqqq1234@mail.ru", "Ivanov Ivan");
         hashMap.put("kastruly@mail.ru", "Kastruly Julia");
         hashMap.put("taras@mail.ru", "Don Taras");
-        hashMap.put("qazwsx@mail.ru", "Bez Prekras");
+        hashMap.put("qazwsx@mail.ru", "Samokat Velosipedov");
         hashMap.put("foodmaster@mail.ru", "Albedo BezObeda");
+        hashMap.put("kastruly@mail.ru", "Kastruly Skovorodovna");
         for (String key : hashMap.keySet()) {
             String value = hashMap.get(key);
             System.out.println(key + " --- " + value);

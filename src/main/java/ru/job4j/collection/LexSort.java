4908e5053a25ca9a295a7d9961ff863795e2943f
Sumyork <psychohistorian.p@gmail.com>
2024-02-19 23:38:48 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        String[] wordsLeft = left.split(". ");
+        String[] rightLeft = right.split(". ");
+        Integer first = Integer.parseInt(wordsLeft[0]);
+        Integer second = Integer.parseInt(rightLeft[0]);
+        return first.compareTo(second);
+    }
+}

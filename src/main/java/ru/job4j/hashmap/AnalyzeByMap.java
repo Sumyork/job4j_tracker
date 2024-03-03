7216c6bbf64a18f69b77511a7eec1a9d49e7bdca
Sumyork <psychohistorian.p@gmail.com>
2024-03-03 22:24:18 +0300

    9. Лямбда выражения и Collection Framework [#505026] - задание.1

--- src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -34,21 +34,6 @@ public class AnalyzeByMap {
         return labels;
     }
 
-//    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
-//        Map<String, Integer> gradesBySubject = new LinkedHashMap<>();
-//        List<Label> labels = new ArrayList<>();
-//        for (Pupil pupil : pupils) {
-//            List<Subject> subjects = pupil.subjects();
-//            for (Subject subject : subjects) {
-//                gradesBySubject.put(subject.name(), gradesBySubject.getOrDefault(subject.name(),  0) + subject.score());
-//            }
-//        }
-//        for (Map.Entry<String, Integer> entry : gradesBySubject.entrySet()) {
-//            labels.add(new Label(entry.getKey(), (double) entry.getValue() / pupils.size()));
-//        }
-//        return labels;
-//    }
-
     public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
         Map<String, Integer> gradesBySubject = new LinkedHashMap<>();
         List<Label> labels = new ArrayList<>();
